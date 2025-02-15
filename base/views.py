d5037937c5d819308827f788dbf0d34f00d6d6c7
kiptoo <brianrtt@gmail.com>
2025-02-15 13:27:55 +0300

    updates

--- base/views.py
+++ base/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from .models import Room,Topic,Message
-from .forms import RoomForm
+from .forms import RoomForm,UserForm
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
@@ -98,19 +98,25 @@ def userProfile(request,pk):
 @login_required(login_url='login')
 def createRoom(request):
     form=RoomForm()
+    topics=Topic.objects.all()
     if request.method=='POST':
-       form=RoomForm(request.POST) 
-       if form.is_valid():
-           room=form.save(commit=False)
-           room.host=request.user
-           room.save()
-           return redirect('home')
-    context={'form':form}
+       topic_name=request.POST.get('topic')
+       topic,created=Topic.objects.get_or_create(name=topic_name)
+
+       Room.objects.create(
+           host=request.user,
+           topic=topic,
+           name=request.POST.get('name'),
+           description=request.POST.get('description'),
+       ) 
+       return redirect('home')
+    context={'form':form,'topics':topics}
     return render(request,'base/room_form.html',context)
 
 @login_required(login_url='login')
 def updateRoom(request,pk):
     room=Room.objects.get(id=pk)
+    topics=Topic.objects.all()
     form=RoomForm(instance=room)
 
     if request.user != room.host:
@@ -121,7 +127,7 @@ def updateRoom(request,pk):
         if form.is_valid():
             form.save()
             return redirect('home')
-    context={'form':form}
+    context={'form':form,'topics':topics}
     return render(request,'base/room_form.html',context)
 
 @login_required(login_url='login')
@@ -146,5 +152,14 @@ def deleteMessage(request,pk):
          return redirect('home')
      return render(request,'base/delete.html',{'obj':message})
 
+@login_required(login_url='login')
+def updateUser(request):
+    user=request.user
+    form=UserForm(instance=user)
 
-    
+    if request.method == 'POST':
+        form=UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile',pk=user.id)
+    return render(request,'base/update-user.html',{'form':form}) 
