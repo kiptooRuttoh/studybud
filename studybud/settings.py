1744512572e642006bb34b20bf51574ca84afd4d
kiptoo <brianrtt@gmail.com>
2025-03-02 07:36:28 +0300

    updates

--- studybud/settings.py
+++ studybud/settings.py
@@ -38,10 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     
-    "base.apps.BaseConfig"
+    "base.apps.BaseConfig",
+    "rest_framework",
+    "corsheaders",
+
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -128,3 +132,5 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS=True
